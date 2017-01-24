2266878f2f8cfd68e9ef3d96f4346bee5f82989e
Cristaliza <sistemas@cristaliza.com>
2017-01-24 19:33:11 +0100

    iOS plugin passing parameters

--- src/ios/geoLocationSdkPlugin.m
+++ src/ios/geoLocationSdkPlugin.m
@@ -1,9 +1,9 @@
 #import "geoLocationSdkPlugin.h"
 #import "FG_Tracking.h"
 #import <Cordova/CDV.h>
-
+#import <GeoQSDK/GeoQSDK.h>
 @implementation geoLocationSdkPlugin
-- (void)locationAction : (CDVInvokedUrlCommand*)command{      //Value of all the parameter from the js file comes in this function.
+- (void)locationAction : (CDVInvokedUrlCommand*)command{
     NSLog(@"init called");
    //  NSString *responseString = [NSString stringWithFormat:@"Hello %@", [command.arguments objectAtIndex:0]];
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Working"];
@@ -11,16 +11,17 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     NSString *javaScript = [pluginResult toSuccessCallbackString:@"callMyJS"];
     NSMutableDictionary* myarg = [command.arguments objectAtIndex:0];
-    NSLog(@"APIKEY%@",[myarg objectForKey:@"geoLocationApiKey"]);// Fetching and printing API key value form the js file by locationAction function.
-    NSLog(@"Notification%@",[myarg objectForKey:@"geoLocationNotification"]); // Fetching and printing Notification value form the js file by locationAction function.
-    NSLog(@"Platform%@",[myarg objectForKey:@"OSPlatform"]); // Fetching and printing Platform value form the js file by locationAction function.
+    //NSLog(@"APIKEY %@",[myarg objectForKey:@"geoLocationApiKey"]);
+    //NSLog(@"Notification %@",[myarg objectForKey:@"geoLocationNotification"]);
+    //NSLog(@"Platform %@",[myarg objectForKey:@"OSPlatform"]);
+
+    
+    [[GeoQManager sharedManager] initDevice:[myarg objectForKey:@"geoLocationApiKey"] andToken:[myarg objectForKey:@"geoLocationNotification"] andPlatform:[myarg objectForKey:@"OSPlatform"]];
     
     
-    
-    
-//    self.trackingManager = [[TrackingManager alloc] init];
-//    [self.trackingManager platformRegistrationWithAPIKey:@"Xa7UMeci8AQA9edo9ABedIVi5OmI4i" andToken:@"RellenarParaEnvioDeNotificaciones" andPlatform:@"http://microsoa.platform.cristaliza.com"];
-//    [self.trackingManager startTracking];
+   
+   // Instance object for GeoQManager and calling start tracking method.
+    [[GeoQManager sharedManager] startTracking];
     
    }
 @end
